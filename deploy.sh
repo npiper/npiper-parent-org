8cb6b512ab7cf6457aceb1bf23bcb9bef4106d06
npiper <solveapuzzledev@gmail.com>
2018-10-10 07:17:07 +0100

    Updated deploy script to use maven settings.xml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
-mvn clean install site deploy scm:tag -Drevision=${TRAVIS_BUILD_NUMBER}.$(git rev-parse --short HEAD) -Dusername=${GIT_USER_NAME} -Dpassword=${GITPW}
+cp .travis.settings.xml $HOME/.m2/settings.xml && mvn clean install site deploy scm:tag -Drevision=${TRAVIS_BUILD_NUMBER}.$(git rev-parse --short HEAD) -Dusername=${GIT_USER_NAME} -Dpassword=${GITPW}
