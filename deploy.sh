3d5b742c672b4e23a7d5de1e21a727c023552344
npiper <solveapuzzledev@gmail.com>
2018-10-10 21:18:41 +0100

    Removed deployment

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git config --global user.name "${GIT_USER_NAME}"
-git config --global user.email "${GIT_USER_EMAIL}"
-cp .travis.settings.xml $HOME/.m2/settings.xml && mvn clean install site deploy scm:tag -Drevision=${TRAVIS_BUILD_NUMBER}.$(git rev-parse --short HEAD) -Dusername=${GIT_USER_NAME} -Dpassword=${GITPW}
